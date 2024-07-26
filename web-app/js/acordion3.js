f073c60c3f4b344876c96e80ab31110c5ff95200
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2024-07-26 14:38:43 -0500

    Add Techician

---
+++ web-app/js/acordion3.js
@@ -0,0 +1,11 @@
+document.getElementById('darkModeToggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
+
+document.getElementById('hamburger').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('open');
+});
+
+function loadPage(page) {
+    document.getElementById('iframeContent').src = page;
+}
