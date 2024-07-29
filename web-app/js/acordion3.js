448477754b8b816a713bb23f23443bfd1f487d17
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2024-07-28 21:03:36 -0500

    Front End

--- web-app/js/acordion3.js
+++ web-app/js/acordion3.js
@@ -1,10 +1,10 @@
-document.getElementById('darkModeToggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-});
+// document.getElementById('darkModeToggle').addEventListener('click', function() {
+//     document.body.classList.toggle('dark-mode');
+// });
 
-document.getElementById('hamburger').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.toggle('open');
-});
+// document.getElementById('hamburger').addEventListener('click', function() {
+//     document.getElementById('sidebar').classList.toggle('open');
+// });
 
 function loadPage(page) {
     document.getElementById('iframeContent').src = page;
