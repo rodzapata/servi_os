2ba26ff4e76e17fe8382b1a29807d7bb3f32a20b
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2024-07-31 07:58:45 -0500

    Add Sidebar

---
+++ web-app/js/eje2.js
@@ -0,0 +1,8 @@
+function loadContent(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('main-content').innerHTML = data;
+        })
+        .catch(error => console.error('Error al cargar la página:', error));
+}
---
+++ web-app/js/scripts.js
@@ -0,0 +1,37 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn= document.querySelector("#close-btn");
+const themetoggler= document.querySelector(".theme-toggler");
+
+
+menuBtn.addEventListener('click',()=>{
+    sideMenu.style.display='block';
+
+})
+
+closeBtn.addEventListener('click',()=>{
+    sideMenu.style.display='none';
+    
+})
+
+themetoggler.addEventListener('click',()=>{
+    document.body.classList.toggle('dark-theme-variables');
+
+    themetoggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themetoggler.querySelector('span:nth-child(2)').classList.toggle('active');
+
+})
+
+function loadContent(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('main-content').innerHTML = data;
+        })
+        .catch(error => console.error('Error al cargar la página:', error));
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadContent('dashboard.html');
+});
