fe8c7bd66c7cb2a8ed67b9b6e2b2ac657515eee9
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2024-07-31 17:20:00 -0500

    modify main with cors

--- src/main/java/com/serviparamo/api_rest/ApiRestApplication.java
+++ src/main/java/com/serviparamo/api_rest/ApiRestApplication.java
@@ -2,6 +2,9 @@ package com.serviparamo.api_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiRestApplication {
@@ -9,6 +12,16 @@ public class ApiRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
 
 }
 
